af1db6df19fe5ff66dc3ed53193178844878de50
Galina684 <galina684@gmail.com>
2023-09-05 00:32:49 +0300

    Домашка 4.

---
+++ src/main/java/pro/sky/springcalculatorgalina684/SpringCalculatorGalina684Application.java
@@ -0,0 +1,13 @@
+package pro.sky.springcalculatorgalina684;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCalculatorGalina684Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCalculatorGalina684Application.class, args);
+	}
+
+}
